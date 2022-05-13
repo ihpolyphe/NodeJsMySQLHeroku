df0fd358b6a0528604068023e1ca19156bb1fe38
ihpolyphe <hayato.8124950@mineo.jp>
2022-05-13 22:50:05 +0900

    change

--- index.js
+++ index.js
@@ -3,39 +3,27 @@ const app = express();
 const bodyParser = require("body-parser");
 var mysql = require("mysql");
 
+app.use(express.static("views"));
+var port = process.env.PORT || 5000;
+
 const connection = mysql.createConnection({
   host: "us-cdbr-east-05.cleardb.net",
   user: "b3449eedb4dad1",
   password: "ffdb1a08",
   database: "heroku_3cda4829cc2bb1d",
 });
-var port = process.env.PORT || 5000;
 
-app.use(express.static("public"));
-
-//Form設定
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.post("/form", urlencodedParser, (req, res) => {
-  console.log(req.body);
-  const name = req.body.name;
-  console.log("name->" + name);
-  res.send("こんにちわ!" + name + "さん");
+// データベースに接続できたらコンソールにConnectedを表示
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
 });
 
-// FetchAPI設定
-const jsonParser = bodyParser.json();
-
-app.post("/fetch", jsonParser, (req, res) => {
-  console.log(req.body);
-  const name = req.body.name;
-  console.log("name->" + name);
-  res.send("こんにちわ!" + name + "さん");
-});
-
-connection.query("SELECT * FROM test", (error, results) => {
-  res.send("Pokemon No." + result[0].id + "Pokemon Name" + result[0].name);
-  console.log(results);
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM pokemonzukan", (error, results) => {
+    res.render("hello.ejs", { pokemonzukan: results });
+    console.log(results[0].id);
+  });
 });
 
 app.listen(port, () => {
