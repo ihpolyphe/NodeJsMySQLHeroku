aa18b5a068b8e0be56cbcf2f439a71060bd5aa20
ihpolyphe <hayato.8124950@mineo.jp>
2022-05-13 21:30:37 +0900

    connection MySQL

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+var mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b3449eedb4dad1",
+  password: "ffdb1a08",
+  database: "heroku_3cda4829cc2bb1d",
+});
 var port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
@@ -24,6 +31,11 @@ app.post("/fetch", jsonParser, (req, res) => {
   const name = req.body.name;
   console.log("name->" + name);
   res.send("こんにちわ!" + name + "さん");
+});
+
+connection.query("SELECT * FROM test", (error, results) => {
+  res.send("Pokemon No." + result[0].id + "Pokemon Name" + result[0].name);
+  console.log(results);
 });
 
 app.listen(port, () => {
