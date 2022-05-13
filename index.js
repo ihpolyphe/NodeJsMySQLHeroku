79d1684973980a572336b3a92366e0308d799d29
ihpolyphe <hayato.8124950@mineo.jp>
2022-05-13 21:44:25 +0900

    install mysql

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "us-cdbr-east-05.cleardb.net",
-//   user: "b3449eedb4dad1",
-//   password: "ffdb1a08",
-//   database: "heroku_3cda4829cc2bb1d",
-// });
+const connection = mysql.createConnection({
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b3449eedb4dad1",
+  password: "ffdb1a08",
+  database: "heroku_3cda4829cc2bb1d",
+});
 var port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
@@ -33,10 +33,10 @@ app.post("/fetch", jsonParser, (req, res) => {
   res.send("こんにちわ!" + name + "さん");
 });
 
-// connection.query("SELECT * FROM test", (error, results) => {
-//   res.send("Pokemon No." + result[0].id + "Pokemon Name" + result[0].name);
-//   console.log(results);
-// });
+connection.query("SELECT * FROM test", (error, results) => {
+  res.send("Pokemon No." + result[0].id + "Pokemon Name" + result[0].name);
+  console.log(results);
+});
 
 app.listen(port, () => {
   console.log("Start server port:5000");
